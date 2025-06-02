ae104c9715fcb4e82f67ae545e3b0091440fb1db
MR-SAMOL <sld999kh@gmail.com>
2025-06-02 22:05:02 +0700

    Lesson-1

---
+++ Lesson-1.cpp
@@ -0,0 +1,19 @@
+// In this lesson we study about [ cout and cin ]
+#include<iostream>
+using namespace std;
+int main(){
+    string name,gender,address;
+    int age,id;
+    // 1. Enter is  cin
+    cout<<"What is Your Name ? :";cin>>name;
+    // 2. Display is cout
+    cout<<"My Name is :"<<name<<endl; 
+    // HOMEWORK FOR YOU
+    // Enter and Display the 
+    // 1. gender
+    // 2. address
+    // 3. age
+    // 4. id   
+}
+
+
