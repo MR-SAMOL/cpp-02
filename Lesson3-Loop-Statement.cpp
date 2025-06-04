0b73bb538842f6c9bbd4d92193e32692e1d431b4
MR-SAMOL <sld999kh@gmail.com>
2025-06-04 21:18:58 +0700

    New update

---
+++ Lesson3-Loop-Statement.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using  namespace std;
+int main(){
+    int s;
+    cout<<"Enter Your Size ! :";cin>>s;
+    for(int i=0;i<s;i++){
+        cout<<"I'm Sorry Baby !"<<i+1<<endl;
+    }
+    return 0;
+}
