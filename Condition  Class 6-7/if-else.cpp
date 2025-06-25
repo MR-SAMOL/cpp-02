b4220a56c29e02655fe1840fa6990564237fc8d0
MR-SAMOL <sld999kh@gmail.com>
2025-06-25 20:20:04 +0700

    cpp

---
+++ Condition  Class 6-7/if-else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    system("cls");
+    int a,b;
+    cout<<"---> Input <---"<<endl;
+    cout<<"Enter The Value A :";cin>>a;
+    cout<<"Enter The Value B :";cin>>b;
+    cout<<"---> Output <---"<<endl;
+    if(a>b){
+        cout<<"The value A Is Big !"<<endl;
+    }else{
+        cout<<" VALUE ERROR !!"<<endl;
+    }
+
+    return 0;
+}
