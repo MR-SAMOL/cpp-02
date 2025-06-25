fa0ba812fd0ca53f7cc64ac48c07524eab5b5611
MR-SAMOL <sld999kh@gmail.com>
2025-06-25 19:10:40 +0700

    cpp

---
+++ Condition 6-7/condition.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// condition
+	
+	int a,b;
+	a=10;
+	b=20;
+	if(a>b){
+		cout<<"A is smaller than B "<<endl;
+	}else if(a==b){
+		cout<<"B is bigger than A"<<endl;
+	}else{
+		cout<<"Please Try agrian !"<<endl;
+	}
+	
+	return 0;
+}
