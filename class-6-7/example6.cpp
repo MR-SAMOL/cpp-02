197fa4d8bd8ee51e89f473a902d3ddcb5fa4056f
MR-SAMOL <sld999kh@gmail.com>
2025-06-25 15:35:58 +0700

    cpp

---
+++ class-6-7/example6.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+//      លំហាត់អនុវត្តន៍ទី​ ៦ មេរៀនទី១​ c++ programming
+
+int main(){
+    cout<<"1\n"<<endl;
+
+    cout<<" A"<<endl;
+    cout<<"A A"<<endl;
+    cout<<"A A A"<<endl<<endl;
+    
+    cout<<"2\n\n"<<endl;
+
+    cout<<"A"<<endl;
+    cout<<"A A"<<endl;
+    cout<<"A A A"<<endl<<endl;
+
+    cout<<"3\n\n"<<endl;
+
+    cout<<"A A A"<<endl;
+    cout<<"A A"<<endl;
+    cout<<"A"<<endl<<endl;
+
+    cout<<"4\n\n"<<endl;
+
+    cout<<" A"<<endl;
+    cout<<"A A"<<endl;
+    cout<<"A A A"<<endl;
+    cout<<"A A A"<<endl;
+    cout<<" A A"<<endl;
+    cout<<"  A"<<endl;
+
+    return 0;
+}
---
+++ class-6-7/example8.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float d,r;
+
+    cout<<"Enter $ :";cin>>d;
+    cout<<"Enter R :";cin>>r;
+
+    cout<<"Change Dollar To Reil :"<<(d*4100)<<"R"<<endl;
+    cout<<"Change Reil To Dollar :"<<(r/4100)<<"$"<<endl;
+
+    return 0;
+}
